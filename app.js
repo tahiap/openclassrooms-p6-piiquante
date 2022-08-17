ae80696129a9d1811a927e90aa2ebfa960a3930c
Tahia Pruvoost <tahia.pruvoost@gmail.com>
2022-08-17 02:19:06 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,37 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const path = require("path")
+
+const userRoutes = require("./routes/user")
+const saucesRoutes = require("./routes/sauce")
+
+mongoose
+	.connect(
+		"mongodb+srv://tahia:h89EX0ZvlLU3tFjQ@clusteroc.clibtr6.mongodb.net/?retryWrites=true&w=majority",
+		{ useNewUrlParser: true, useUnifiedTopology: true }
+	)
+	.then(() => console.log("Connexion à MongoDB réussie !"))
+	.catch(() => console.log("Connexion à MongoDB échouée !"))
+
+const app = express()
+
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*")
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+	)
+	res.setHeader(
+		"Access-Control-Allow-Methods",
+		"GET, POST, PUT, DELETE, PATCH, OPTIONS"
+	)
+	next()
+})
+
+app.use(express.json())
+
+app.use("/api/auth", userRoutes)
+app.use("/api/sauces", saucesRoutes)
+app.use("/images", express.static(path.join(__dirname, "images")))
+
+module.exports = app
